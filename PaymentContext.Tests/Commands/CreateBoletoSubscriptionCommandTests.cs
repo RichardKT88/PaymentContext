645089d42ff2da19a1242ba33bc038353085847a
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-12-23 13:11:14 -0300

    Aula 17 - Testando os Commands

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,16 @@
+namespace PaymentContext.Domain.Commands
+{
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        //Red, Green, Refactor
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+           var command = new CreateBoletoSubscriptionCommand();
+           command.FirstName = "";
+
+           command.Validate();
+           Assert.AreEqual(false, command.Valid);
+        }
+    }
+}
